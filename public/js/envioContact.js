87933d21040cd572f8bd35dc56569d4afe3dc676
jp <pablo91128@gmail.com>
2024-03-13 22:24:01 -0300

    v 0.86
    text

--- public/js/envioContact.js
+++ public/js/envioContact.js
@@ -10,7 +10,7 @@ const envio = async()=>{
         const response = await fetch('https://www.casadecoach.cl/contacto?',{
             method: 'POST',
             headers: {
-                'Content-Type': 'application/json',
+                'Content-Type':'application/json',
             },
             body: JSON.stringify(formData),
         });
